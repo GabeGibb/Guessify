ad99abd66f2028b107254fb931ee080323ac58d7
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-01-06 16:33:00 -0800

    oops

--- client/src/services/Url.js
+++ client/src/services/Url.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://api.example.com' : 'http://localhost:1323/';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://guessify-backend.onrender.com/' : 'http://localhost:1323/';
 
 export default baseUrl;
