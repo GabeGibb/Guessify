b15bf9f88a92d4a8b8e0afa8dfba70a1256a3040
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-01-06 19:19:19 -0800

    asd

--- client/src/index.js
+++ client/src/index.js
@@ -19,7 +19,7 @@ export default function App() {
                     <Route path="/" element={<MainPage />} />
                     <Route path="/game" element={<GamePage />} />
                     <Route path="/home" element={<HomePage />} />
-                    <Route path="*" element={<MainPage />} /> {/*ERROR*/}
+                    {/* <Route path="*" element={<MainPage />} /> ERROR */}
                 </Routes>
             </BrowserRouter>
             </>
