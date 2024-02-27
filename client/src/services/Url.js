b0acb27ab3f9305014c8da5dd535c53e36c6dda3
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-02-26 22:06:50 -0800

    CORS I HATE CORS

--- client/src/services/Url.js
+++ client/src/services/Url.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://guessify-backend.onrender.com/api/' : 'http://localhost:1323/';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://guessify-backend.onrender.com/api/' : 'http://localhost:1323/api/';
 // https://guessify-red.vercel.app/'
 export default baseUrl;
