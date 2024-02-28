faf5a4052ac191d8104e594104f4023aef47e9ef
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-02-27 21:53:02 -0800

    updates

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -44,7 +44,7 @@ const Popup = ({ gameOver, score, song, restartCallback, artistPicUrl='' }) => {
         
     //         console.log(window.innerWidth)
     //     }
-    // }
+    // 
 
     // useEffect(() => {
     //     window.addEventListener('resize', changeScale);
@@ -58,10 +58,10 @@ const Popup = ({ gameOver, score, song, restartCallback, artistPicUrl='' }) => {
         <div>
             <div className='fixed w-full h-full left-0 top-0 bg-black/80 z-20'>
                 <div className='w-full absolute top-[10%] left-1/2 transform -translate-x-1/2'>
-                    <div className='relative max-w-[675px] sm:w-auto m-auto'>
+                    <div className='relative max-w-[600px] sm:w-[80%] m-auto'>
                         <div className='absolute left-0 top-0 flex flex-col justify-evenly h-full w-full z-10'>
-                            <h1 className='text-5xl sm:text-3xl font-bold w-[75%] m-auto'>Nice try!</h1>
-                            <div className='w-[75%] m-auto flex flex-col text-2xl'>
+                            <h1 className='text-4xl sm:text-3xl font-bold w-[75%] m-auto'>Nice try!</h1>
+                            <div className='w-[75%] m-auto flex flex-col text-2xl sm:text-lg'>
                                 <div className='inline-flex gap-2'>
                                     <h2>Song: </h2><h2>{song.name}</h2>
                                 </div>
@@ -69,7 +69,7 @@ const Popup = ({ gameOver, score, song, restartCallback, artistPicUrl='' }) => {
                                     <h2>Artist: </h2><h2>{song.artists[0].name}</h2>
                                 </div>
                             </div>
-                            <div className='w-[75%] m-auto flex flex-row text-2xl'>
+                            <div className='w-[75%] m-auto flex flex-row justify-between sm:flex-col text-xl sm:text-lg'>
                                 <div className='gap-2 flex items-center'>
                                     <h2>Final Score: </h2>
                                     <h2>{score}</h2>
@@ -77,17 +77,17 @@ const Popup = ({ gameOver, score, song, restartCallback, artistPicUrl='' }) => {
                                 {/* <div className='inline-flex gap-2'>
                                     <h2>Highest Score: </h2><h2>{score}</h2>
                                 </div> */}
-                                <div className='flex m-auto md:scale-90 left-0'>
+                                <div className='flex md:scale-[70%]'>
                                     <MysterySong song={song} delay={30}/>
                                 </div>
                             </div>
-                            <div className='w-[75%] m-auto inline-flex justify-between'>
-                                <button className='w-[45%] text-3xl bg-[var(--spotify-green)] px-10 py-5 rounded-2xl' onClick={handleRefresh}>Try Again</button>
-                                <button className='w-[45%] text-3xl bg-[#3D3E3F] px-10 py-5 rounded-2xl' onClick={handleGoHome}>Home</button>
+                            <div className='w-[75%] m-auto inline-flex sm:flex-col sm:space-y-2 justify-between'>
+                                <button className='w-[45%] sm:w-auto text-2xl sm:text-xl bg-[var(--spotify-green)] px-10 py-5 rounded-2xl' onClick={handleRefresh}>Try Again</button>
+                                <button className='w-[45%] sm:w-auto text-2xl sm:text-xl bg-[#3D3E3F] px-10 py-5 rounded-2xl' onClick={handleGoHome}>Home</button>
                             </div>
                         </div>
-                        <div className='w-full bg-[#3D3E3F] border-white rounded-3xl border'>
-                            {<div className='w-full aspect-[1] overflow-hidden rounded-3xl'>
+                        <div className='w-full h-full bg-[#3D3E3F] border-white rounded-3xl border'>
+                            {<div className='w-full h-full overflow-hidden rounded-3xl'>
                                 <img className='w-full h-full object-cover object-center opacity-30' src={picUrl} alt='Popup Background'/>
                             </div>}
                         </div>
