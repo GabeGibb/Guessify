047309cbf43070964dd1e7dbc2a1eb561e7c08b0
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-02-27 22:46:41 -0800

    last thing

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 const Loading = () => {
     return (
-        <div className='flex justify-center items-center h-screen'>
+        <div className='flex justify-center items-start translate-y-[20%] h-screen'>
             <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-[#1fd15e]'></div>
         </div>
     );
