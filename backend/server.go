adabfeacde74a357066a48027434214a901e31fd
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-02-27 21:05:00 -0800

    test

--- backend/server.go
+++ backend/server.go
@@ -34,7 +34,7 @@ func setupConfig() {
 	}
 	store = sessions.NewCookieStore([]byte("secret"))
 	store.Options = &sessions.Options{
-		Path:     "/",
+		Path:     os.Getenv("FRONTEND_URL"),
 		MaxAge:   86400, // 1 day
 		HttpOnly: true,
 		SameSite: http.SameSiteNoneMode,
