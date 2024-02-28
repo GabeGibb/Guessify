d816bae91450b450ce3b6e47dd69ae6876bd8073
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-02-27 20:43:01 -0800

    revert

--- client/src/services/Url.js
+++ client/src/services/Url.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://guessify-red.vercel.app/api/' : 'http://localhost:1323/';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://guessify-red.vercel.app/api/' : 'https://guessify-red.vercel.app/api/';
 // https://guessify-red.vercel.app/'
 export default baseUrl;
